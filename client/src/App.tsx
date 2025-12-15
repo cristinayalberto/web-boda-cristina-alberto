166ee0e81f7f4ecefb9ef53d56f0c5645bd5331e
ludopator <pgrosillo@gmail.com>
2025-12-15 23:32:51 +0000

    Fix router and deploy updated build to GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,24 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Wedding from "@/pages/Wedding";
+import NotFound from "@/pages/not-found";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Switch>
-          <Route path="/" component={Wedding} />
-        </Switch>
+
+        {/* GitHub Pages: el site vive en /web-boda-cristina-alberto/ */}
+        <Router base="/web-boda-cristina-alberto">
+          <Switch>
+            <Route path="/" component={Wedding} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
