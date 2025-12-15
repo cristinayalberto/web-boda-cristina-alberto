bc25c7b8a9604a7f42d37e6340f6bad848493fd4
ludopator <pgrosillo@gmail.com>
2025-12-15 23:29:21 +0000

    Fix router and deploy updated build to GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,14 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Wedding from "@/pages/Wedding";
-import NotFound from "@/pages/not-found";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-
         <Switch>
           <Route path="/" component={Wedding} />
-          <Route component={NotFound} />
         </Switch>
       </TooltipProvider>
     </QueryClientProvider>
